5d5264cd0a18609291dbdb6c3200e21e254d315d
DavevdDrift <43164376+DavevdDrift@users.noreply.github.com>
2019-03-11 15:10:14 +0100

    Update Script.js

--- Scripts/Script.js
+++ Scripts/Script.js
@@ -66,7 +66,7 @@ function checkTime(i) {
 
 
 var d = new Date();
-if(d.getHours() >= 6 && d.getHours() < 18 ){
+if(d.getHours() >= 6 && d.getHours() < 12 ){
     $(".body1").show();
     $(".body2").hide();
     
